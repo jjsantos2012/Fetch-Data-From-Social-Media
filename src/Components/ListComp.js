325279430b8877e06903bb4253b42f76313641fa
Juan José de los santos peguero <jjsantos2012@hotmail.es>
2018-02-24 19:06:35 -0400

    adding input field to make dinamic search
    here am adding the method to make the search functionality works, I also modify some default css and added another one to make the list of recors not static.

--- src/Components/ListComp.js
+++ src/Components/ListComp.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import {Card, Container, CardTitle, Col, Row} from 'reactstrap';
+import './CSS/publicacion.css';
+import {Card,Container, CardTitle, Col } from 'reactstrap';
+import {Row,Form, FormGroup, Input, Button,Label } from 'reactstrap';
+
 
 class ListComp extends Component {
 
@@ -7,7 +10,14 @@ class ListComp extends Component {
     super(props);
     this.state = {data: [{created_time: "2018-02-23T17:42:32+0000",
        message: "Anunciamos el inicio de nuestro blog. Pueden verlo en https://clubpatassucias.blogspot.com/",
-        id: "1947984875458605_2016570478600044"}]};
+        id: "1947984875458605_2016570478600044"}],
+      criteria:'',
+      value:''
+    };
+
+        this.searchForPosts = this.searchForPosts.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+
   }
 
 componentWillMount(){
@@ -18,26 +28,51 @@ componentWillMount(){
          console.log(json);
          this.setState({data: json});
        });
-    console.log(this.state.data);
 
+}
 
+searchForPosts(criteria){
+
+ var parameter1 = "https://graph.facebook.com/v2.9/";
+ var parameter3 = "/posts?access_token=403108406768407|amJGwlHjGLWXcKOMow0MGmJpTgU";
+ var finalParameter2 = parameter1.concat(criteria).concat(parameter3);
+
+  fetch(finalParameter2)
+       .then(response => response.json())
+       .then(json => {
+         console.log(json);
+         this.setState({data: json});
+       });
+
+}
+
+handleChange(e) {
+  this.setState({ [e.target.name]: e.target.value});
 }
 
 
   render() {
       if (this.state.data['data']) {
      return (
+
        <Container>
+         <Form>
+        <FormGroup>
+          <Label for="exampleEmail">Page ID:</Label>
+          <Input type="text" name="criteria" required="true" onChange={this.handleChange} value={this.state.criteria} placeholder="Add the Page ID you what to search..." />
+            <Button color="primary" onClick={() => {this.searchForPosts(this.state.criteria)}}>Search</Button>
+        </FormGroup>
+        </Form>
 
        {this.state.data['data'].map((item) => {
                return (
-                 <div style={{marginRight: "5%", marginLeft: "5%"}}>
+                 <div key={item.id}style={{marginRight: "5%", marginLeft: "5%"}}>
                  <Card style={{background: "rgba(255, 255, 255, 0.94)", cursor: "pointer"}}>
              				<Row>
                       <Col>
+                        <h4>{item.story}</h4>
                         <h4>{item.message}</h4>
                       </Col>
-             					<hr/>
              				</Row>
              		</Card>
                 </div>
